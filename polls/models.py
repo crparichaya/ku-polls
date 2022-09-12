78afb31201f90320e5b06ffabeb81735e3972873
Parichaya Yangsiri <parichaya.yan@ku.th>
2022-09-12 16:44:28 +0700

    Adding end_date attribute and two methods

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    end_date = models.DateTimeField('date ended', default=None, null=True)
 
     def __str__(self):
         return self.question_text
@@ -14,6 +15,16 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.localtime()
+        return self.pub_date <= now
+
+    def can_vote(self):
+        now = timezone.localtime()
+        if self.end_date is None:
+            return self.pub_date <= now
+        return self.pub_date <= now <= self.end_date
 
 
 class Choice(models.Model):
@@ -23,6 +34,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-
-
