05c51e64d5a76fb9405ba83206836877d3ff39f2
Parichaya Yangsiri <parichaya.yan@ku.th>
2022-10-18 01:21:05 +0700

    fixed

--- polls/tests.py
+++ polls/tests.py
@@ -148,6 +148,7 @@ class QuestionDetailViewTests(TestCase):
         The detail view of a question with a pub_date in
         the past displays the question's text.
         """
+        self.client.login(username="AAA", password="aaa")
         past_question = create_question(question_text='Past Question.', days=-5)
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
